3d0ae750f6732881e7982311788c664173c21a8a
Jeroen P. Broks <facebook@tbbs.nl>
2018-10-19 11:30:08 +0200

    Add vertexes

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
 // Script
 
+const rand(mn,mx) => {
+  let verschil=mx-mn;
+  let r=(Math.random()*verschil)+mn;
+  return r;
+}
+
+class vertex(){
+  constructor(px,py,mydemo){
+    this.x=px;
+    this.y=py;
+    if (mydemo) mydemo.add(vertex);
+  }
+}
+
+class demo{
+  constructor(hoeveel,w,h){
+    this.vertexes=[]
+    for (let i=0;i<hoeveel;i++) new vertex(rand(0,w),rand(0,h),this)
+  }
+
+  add(myvertex) { this.vertexes.push(myvertex); }
+}
